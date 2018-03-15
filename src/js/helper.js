b24f9550534033cdc238c27da85205d32a9a4c3d
Gabor Heja <gheja@m002560p>
2018-03-15 22:11:59 +0100

    Helper: now working

--- src/js/helper.js
+++ src/js/helper.js
@@ -9,20 +9,65 @@ class Helper
 {
 	constructor()
 	{
-		this.message = "";
-		this.direction = HELPER_DIRECTION_TOP;
+	}
+	
+	destroy(obj)
+	{
+		obj.parentNode.removeChild(obj);
+	}
+	
+	hide(event)
+	{
+		let obj;
+		
+		obj = event.target;
+		
+		while (obj.parentNode != document.body)
+		{
+			if (obj.parentNode == null)
+			{
+				console.log("Could not find parent");
+				return;
+			}
+			
+			obj = obj.parentNode;
+		}
+		
+		obj.className = "helper";
+		
+		// destroy the element
+		window.setTimeout(this.destroy.bind(this, obj), 1000);
+	}
+	
+	activate(obj)
+	{
+		obj.className = "helper helper_active";
 	}
 	
 	showAtPosition(s, x, y, direction)
 	{
-		this.direction = direction
-		this.message = s;
+		let obj;
 		
-		console.log(this.message);
+		obj = document.createElement("div");
+		
+		obj.className = "helper";
+		obj.style.top = Math.round(y - 100) + "px";
+		obj.style.left = Math.round(x) + "px";
+		obj.innerHTML = s + "<span class=\"dim\"> | Click to close.</span>";
+		
+		obj.addEventListener("click", this.hide.bind(this));
+		
+		window.setTimeout(this.activate.bind(this, obj), 100);
+		
+		document.body.appendChild(obj);
 	}
 	
 	showAtObject(s, obj, direction)
 	{
-		this.showAtPosition(s, 100, 100, direction);
+		let position;
+		
+		position = obj.getBoundingClientRect();
+		
+		this.showAtPosition(s, position.x + position.width / 2, position.y, direction);
 	}
 }
